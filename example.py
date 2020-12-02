b00b8b1580c1fae80b369050abf8952f416f9701
Evan Gubarev <evan@evangubarev.com>
2020-12-02 14:47:15 -0800

    Update example.py

--- example.py
+++ example.py
@@ -11,11 +11,11 @@ async def main():
     await client.login()
     for school_class in client.classes:
         print(f"Found class {school_class.name} at https://{client.url_base}{school_class.url}.")
-    # for i in range(len(client.classes)):
-    #     assignments = await client.fetch_assignments(client.classes[i])
-    #     for assignment in assignments:
-    #         print(f"{assignment.name} was{' ' if assignment.handed_in else ' not '}turned in. The url for this assignment is at https://{client.url_base}{assignment.url}")
-    history_grades = await client.get_grades(client.classes[0])
+    for i in range(len(client.classes)):
+        assignments = await client.fetch_assignments(client.classes[i])
+        for assignment in assignments:
+            print(f"{assignment.name} was{' ' if assignment.handed_in else ' not '}turned in. The url for this assignment is at https://{client.url_base}{assignment.url}")
+
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(main())
